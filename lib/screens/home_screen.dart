c8d43ba0f1abf946d7201bde7fb71838cc6f0678
Grandes <ho2708@naver.com>
2022-12-25 15:53:38 +0900

    :technologist: feature(study): study_6 WEBTOON APP #3 - ~ 6.5 waitForWebToons

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -1,10 +1,39 @@
 import 'package:flutter/material.dart';
 
-class HomeScreen extends StatelessWidget {
+import '../models/webtoon_model.dart';
+import '../services/api_service.dart';
+
+class HomeScreen extends StatefulWidget {
   const HomeScreen({super.key});
 
   @override
+  State<HomeScreen> createState() => _HomeScreenState();
+}
+
+class _HomeScreenState extends State<HomeScreen> {
+  List<WebtoonModel> webtoons = [];
+  bool isLoading = true;
+
+  void waitForWebToons() async{
+    webtoons = await ApiService.getTodaysToons();
+    isLoading = false;
+    // screen refresh
+    setState(() {
+
+    });
+  }
+
+  @override
+  void initState() {
+    super.initState();
+    waitForWebToons();
+  }
+
+  @override
   Widget build(BuildContext context) {
+    print(webtoons);
+    print(isLoading);
+
     return Scaffold(
       backgroundColor: Colors.white,
       appBar: AppBar(
@@ -21,5 +50,4 @@ class HomeScreen extends StatelessWidget {
       ),
     );
   }
-
 }
